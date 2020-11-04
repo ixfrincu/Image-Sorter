d5eb4156d977b85be9ac7d9101c8374954d5a192
Ionut Frincu <ixfrincu@gmail.com>
2020-11-04 22:37:35 +0200

    Removed unnecessary formats and fixed some typos

--- imgSort.py
+++ imgSort.py
@@ -7,22 +7,22 @@ from hachoir.metadata import extractMetadata
 
 date_time_tag = 36867
 
-imgFormats = ['png', 'jpg', 'jpeg', 'heif', 'hvec', 'h.264', 'heic', 'aae']
+imgFormats = ['png', 'jpg', 'jpeg']
 videoFormats = ['m4v', 'mov', 'mp4']
 
 months = {
-    "1" : "01.Ianuarie",
-    "2" : "02.Februarie",
-    "3" : "03.Martie",
-    "4" : "04.Aprilie",
-    "5" : "05.Mai",
-    "6" : "06.Iunie",
-    "7" : "07.Iulie",
-    "8" : "08.August",
-    "9" : "09.Septembrie",
-    "10" : "10.Octombri",
-    "11" : "11.Noiembrie",
-    "12" : "12.Decembrie"
+    "1": "01.Ianuarie",
+    "2": "02.Februarie",
+    "3": "03.Martie",
+    "4": "04.Aprilie",
+    "5": "05.Mai",
+    "6": "06.Iunie",
+    "7": "07.Iulie",
+    "8": "08.August",
+    "9": "09.Septembrie",
+    "10": "10.Octombrie",
+    "11": "11.Noiembrie",
+    "12": "12.Decembrie"
 }
 
 for xfile in os.listdir():
@@ -63,7 +63,8 @@ for xfile in os.listdir():
             continue
         for line in metadata.exportPlaintext():
             if line.split(':')[0] == ' - Creation date':
-                dateobj = datetime.datetime.strptime(line.split(':')[1].split[0], "%Y-%m-%d")
+                dateobj = datetime.datetime.strptime(
+                    line.split(':')[1].split[0], "%Y-%m-%d")
                 month = months.get(f'{dateobj.month}')
                 dirpath = f'{dateobj.year}/{month}/{dateobj.day}/'
                 os.makedirs(dirpath, exist_ok=True)
